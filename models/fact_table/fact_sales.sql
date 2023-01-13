9468baddd4d94cb43b29187e2de10f3dc6888812
shahriarsourav2905@gmail.com <shahriarsourav2905@gmail.com>
2023-01-13 13:52:57 +0000

    added sales table

---
+++ models/fact_table/fact_sales.sql
@@ -0,0 +1,10 @@
+WITH SALES AS(
+    SELECT *
+    FROM {{ref('dim_sales')}}
+)
+
+SELECT 
+  *,
+  ROUND(Total_Unit_Cost*Order_Quantity,2) AS Total_Cost,
+  Line_Total-(Total_Unit_Cost*Order_Quantity) AS Total_Revenue
+ FROM sales
